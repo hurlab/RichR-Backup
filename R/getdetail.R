2fe693071b658915ff81a10f4a646dae691f0398
bioguo <guokai8@gmail.com>
2018-07-07 21:39:37 -0500

    Add files via upload

---
+++ R/getdetail.R
@@ -0,0 +1,20 @@
+#' Get detail information from enrichment results
+#' @param rese Functional enrichment results
+#' @param resd DEG result with one gene name column or vector of input genes 
+#' @export
+#' @author Kai Guo
+getdetail<-function(rese,resd){
+  require(tidyverse)
+  if(!is.data.frame(resd)){
+    resd=data.frame(gene=resd)
+    }
+  if(!("gene"%in%colnames(resd))){
+    resd$gene=rownames(resd)
+    }
+  gene<-strsplit(as.vector(rese$GeneID),split="\\,")
+  names(gene)<-rese$Annot
+  gened<-data.frame("TERM"=rep(names(gene),times=unlist(lapply(gene,length))),"Annot"=rep(rese$Term,times=unlist(lapply(gene,length))),"GeneID"=unlist(gene),row.names=NULL)
+  gened$GeneID<-as.character(gened$GeneID)
+  res<-left_join(gened,resd,by=c("GeneID"="gene"))
+  return(res)
+}
